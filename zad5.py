db48046762dfa032a7bab8fa0b602f0dcfc8b43a
sdrapinski <55097897+sdrapinski@users.noreply.github.com>
2024-03-01 12:37:49 +0100

    Update zad5.py

--- zad5.py
+++ zad5.py
@@ -12,8 +12,14 @@ def init_opengl_program(window):
 	# Ładowanie programów cieniujących
 	DemoShaders.initShaders("helpers/shaders/")
 
+sun = Sphere(.5)
+earth = Sphere(.2)
+moon = Sphere(.1)
 
-def draw_scene(window):
+earth2 = Sphere(.25)
+moon2 = Sphere(0.07)
+
+def draw_scene(window,time):
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
 	V = glm.lookAt(
@@ -30,7 +36,39 @@ def draw_scene(window):
 	M = glm.mat4(1.0)
 	glUniformMatrix4fv(DemoShaders.spConstant.u("M"), 1, GL_FALSE, M.to_list())
 
-	# TU RYSUJEMY
+	sun.drawWire()
+	## earth
+	mEarth = glm.mat4(1.0)
+	mEarth *= glm.rotate(glm.radians(60)*time, glm.vec3(0, 1, 0))
+	mEarth *= glm.translate(glm.vec3(1.5, 0, 0))
+
+	glUniformMatrix4fv(DemoShaders.spConstant.u("M"), 1, GL_FALSE, mEarth.to_list())
+	earth.drawSolid()
+
+	## moon
+	mMoon = mEarth
+	mMoon *= glm.rotate(glm.radians(60)*time, glm.vec3(0, 1, 0))
+	mMoon *= glm.translate(glm.vec3(.5, 0, 0))
+	
+	glUniformMatrix4fv(DemoShaders.spConstant.u("M"), 1, GL_FALSE, mMoon.to_list())
+	moon.drawSolid()
+	# Earth 2
+
+	mEarth2 = M
+	mEarth2 *= glm.rotate(glm.radians(60)*time, glm.vec3(0, 0, 1))
+	mEarth2 *= glm.translate(glm.vec3(0, 2, 1))
+
+	glUniformMatrix4fv(DemoShaders.spConstant.u("M"), 1, GL_FALSE, mEarth2.to_list())
+	earth2.drawSolid()
+
+	#Moon 2
+	mMoon2 = mEarth2
+	mMoon2 *= glm.rotate(glm.radians(60)*time, glm.vec3(0, 1, 0))
+	mMoon2 *= glm.translate(glm.vec3(.5, 0, 0))
+	
+	glUniformMatrix4fv(DemoShaders.spConstant.u("M"), 1, GL_FALSE, mMoon2.to_list())
+	moon2.drawSolid()
+
 
 	glfw.swap_buffers(window)
 
@@ -49,7 +87,7 @@ def main():
 	glfw.set_time(0)
 
 	while not glfw.window_should_close(window):
-		draw_scene(window)
+		draw_scene(window,glfw.get_time())
 		glfw.poll_events()
 
 	free_opengl_program(window)
