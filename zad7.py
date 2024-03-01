cbf168858d603eebbada75b0bc7c63efddd30c33
sdrapinski <55097897+sdrapinski@users.noreply.github.com>
2024-03-01 13:00:39 +0100

    Update zad7.py

--- zad7.py
+++ zad7.py
@@ -12,8 +12,11 @@ def init_opengl_program(window):
 	# Ładowanie programów cieniujących
 	DemoShaders.initShaders("helpers/shaders/")
 
+torus = Torus()
+torus2 = Torus()
+cube = Cube()
 
-def draw_scene(window):
+def draw_scene(window,time):
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
 	V = glm.lookAt(
@@ -30,7 +33,28 @@ def draw_scene(window):
 	M = glm.mat4(1.0)
 	glUniformMatrix4fv(DemoShaders.spConstant.u("M"), 1, GL_FALSE, M.to_list())
 
-	# TU RYSUJEMY
+	mTorus = glm.mat4(1.0)
+	mTorus *= glm.translate(glm.vec3(1.2, 0, 0))
+	mTorus *= glm.rotate(glm.radians(60)*time, glm.vec3(0, 0, 1))
+	glUniformMatrix4fv(DemoShaders.spConstant.u("M"), 1, GL_FALSE, mTorus.to_list())
+	torus.drawWire()
+
+	#torus 2
+	mTorus2 = glm.mat4(1.0)
+	mTorus2 *= glm.translate(glm.vec3(-1.2, 0, 0))
+	mTorus2 *= glm.rotate(glm.radians(60)*time, glm.vec3(0, 0, -1))
+	glUniformMatrix4fv(DemoShaders.spConstant.u("M"), 1, GL_FALSE, mTorus2.to_list())
+	torus2.drawWire()
+
+	for x in range(12):
+		Mk = mTorus * glm.rotate(glm.radians(30)*x, glm.vec3(0, 0, 1)) * glm.translate(glm.vec3(1,0,0)) * glm.scale(glm.vec3(0.1,0.1,0.1))
+		glUniformMatrix4fv(DemoShaders.spConstant.u("M"), 1, GL_FALSE, Mk.to_list())
+		cube.drawWire()
+
+	for x in range(12):
+		Mk = mTorus2 * glm.rotate(glm.radians(30)*x, glm.vec3(0, 0, 1)) * glm.translate(glm.vec3(1,0,0)) * glm.scale(glm.vec3(0.1,0.1,0.1))
+		glUniformMatrix4fv(DemoShaders.spConstant.u("M"), 1, GL_FALSE, Mk.to_list())
+		cube.drawWire()
 
 	glfw.swap_buffers(window)
 
@@ -49,7 +73,7 @@ def main():
 	glfw.set_time(0)
 
 	while not glfw.window_should_close(window):
-		draw_scene(window)
+		draw_scene(window,glfw.get_time())
 		glfw.poll_events()
 
 	free_opengl_program(window)
